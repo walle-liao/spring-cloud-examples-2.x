301ddb172cb332a65ff81d50377fd7a0f102417e
liaozhicheng <13480822481@qfang.com>
2019-07-20 11:35:43 +0800

    init

---
+++ server/eureka/src/main/java/com/qfang/examples/spring/cloud/server/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.qfang.examples.spring.cloud.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: liaozhicheng
+ * @date: 2019-07-13
+ * @since 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
